d8c56b894885c960ee716488f0081110470cce36
Erick Chandra <erickchandra718@gmail.com>
2021-10-23 08:54:14 +0700

    update

--- Logic Enhancement/script.js
+++ Logic Enhancement/script.js
@@ -1,1 +1,28 @@
-// 1. 
+// 1. Left triangle pattern
+console.log('1. Left triangle pattern');
+
+for (let i = 1; i <= 10; i++) {
+    console.log('*'.repeat(i));
+}
+
+console.log('');
+
+
+// 2. Downward Triangle Star Pattern
+console.log('2. Downward Triangle Star Pattern');
+
+for (let i = 10; i > 0; i--) {
+    console.log('*'.repeat(i));
+}
+
+console.log('');
+
+
+// 3. Hollow Triangle Star Pattern
+console.log('3. Hollow Triangle Star Pattern');
+
+for (let i = 1; i < 10; i++) {
+
+}
+
+console.log('');
